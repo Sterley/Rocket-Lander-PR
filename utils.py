fbda17b9912f33ec293086254efc9fa4ccc63b3d
Sterley Labady <43834576+Sterley@users.noreply.github.com>
2022-04-08 14:56:38 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,6 +9,7 @@ from sb3m.stable_baselines3 import DDPG, REINFORCE
 import random
 import torch as th
 from sb3m.stable_baselines3.common.utils import obs_as_tensor
+import tensorflow as tf
 
 
 def get_data_list_2D(x_lab, y_lab, filename):
@@ -105,10 +106,11 @@ def plot_critic(model, env, plot=True, figname="vfunction.pdf", foldername="/plo
 
             nb_rewards += 1
             sum_rewards += rewards
-
+            print("obs1", obsvT)
             tmp = []
             tmp.append(obsvT) 
             obsv = np.asarray(tmp)
+            print("obs2", obsv)
 
             value = model.policy.predict_values(obs_as_tensor(obsv, model.device))
             value = value[0][0]
@@ -137,6 +139,8 @@ def plot_critic(model, env, plot=True, figname="vfunction.pdf", foldername="/plo
         r = r*200
         plt.scatter(xr[0],r*15,color='b')
     final_show(save_figure, plot, figname, "Nombre de Env.reset()", "Position Y", "Rewards (Min: "+str(min_r)+", Max: "+str(max_r)+")", foldername)
+
+
 
 
 
@@ -198,45 +202,6 @@ def plot_policy(policy, env, plot=True, save_figure=True) -> None:
     final_show(save_figure, plot, "throttle.pdf", "Nombre de Env.reset()", "Position Y", "Rewards (Min: "+str(min_r)+", Max: "+str(max_r)+")", "/plots/")
 
 
-def episode_to_traj(rollout_data):
-    """
-    Transform the states of a rollout_data into a set of (x,y) pairs
-    :param rollout_data:
-    :return: the (x,y) pairs
-    """
-    x = []
-    y = []
-    # print("rd", rollout_data)
-    obs = rollout_data.observations
-    # print("obs", obs)
-    # TODO : treat the case where the variables to plot are not the first two
-    for o in obs:
-        x.append(o[0].numpy())
-        y.append(o[1].numpy())
-    return x, y
-
-
-def plot_trajectory(rollout_data, env, fig_index, save_figure=True, plot=True) -> None:
-    """
-    Plot the set of trajectories stored into a batch
-    :param rollout_data: the source batch
-    :param env: the environment where the batch was built
-    :param fig_index: a number, to save several similar plots
-    :param save_figure: where the plot should be saved
-    :return: nothing
-    """
-    if env.observation_space.shape[0] < 2:
-        raise (ValueError("Observation space of dimension {}, should be at least 2".format(env.observation_space.shape[0])))
-
-    # Use the dimension names if given otherwise default to "x" and "y"
-    x_label, y_label = getattr(env.observation_space, "names", ["x", "y"])
-
-    x, y = episode_to_traj(rollout_data)
-    plt.scatter(x, y, c=range(1, len(rollout_data.observations) + 1), s=3)
-    figname = "trajectory_" + str(fig_index) + ".pdf"
-    final_show(save_figure, plot, figname, x_label, y_label, "Trajectory", "/plots/")
-    
-   
 def plot_policy_X_Y(policy, env, plot=True, save_figure=True) -> None:
 
     if env.observation_space.shape[0] <= 2:
@@ -266,3 +231,112 @@ def plot_policy_X_Y(policy, env, plot=True, save_figure=True) -> None:
     plt.colorbar(label="throttle")
 
     final_show(save_figure, plot, "throttle.pdf", "Position X", "Position Y", "throttle/x/y", "/plots/")
+
+
+
+def plot_nd_policy2(policy, env, deterministic, plot=True, figname="nd_actor.pdf", save_figure=True) -> None:
+
+    if env.observation_space.shape[0] <= 2:
+        raise (ValueError("Observation space dimension {}, should be > 2".format(env.observation_space.shape[0])))
+    definition = 100
+    portrait = np.zeros((definition, definition))
+    state_min = env.observation_space.low
+    state_max = env.observation_space.high
+    for index_x, x in enumerate(np.linspace(state_min[4], state_max[4], num=definition)):
+        for index_y, y in enumerate(np.linspace(state_min[3], state_max[3], num=definition)):
+            obs = np.array([[]])
+            for _ in range(0, 1):
+                z = random.random() - 0.5
+                obs = np.append(obs, z)
+            obs = np.append(obs, y)
+            for _ in range(0, 0):
+                z = random.random() - 0.5
+                obs = np.append(obs, z)
+            obs = np.append(obs, x)
+            for _ in range(3, len(state_min)):
+                z = random.random() - 0.5
+                obs = np.append(obs, z)
+            action, _ = policy.predict(obs, deterministic=deterministic)
+            #portrait[definition - (1 + index_y), index_x] = action[1]
+            portrait[definition - (1 + index_y), index_x] = action[0]
+    plt.figure(figsize=(10, 10))
+    plt.imshow(portrait, cmap="inferno", extent=[state_min[0], state_max[0], state_min[1], state_max[1]], aspect="auto")
+    plt.colorbar(label="Cardan Moteur")
+    # Add a point at the center
+    plt.scatter([0], [0])
+    x_label, y_label = getattr(env.observation_space, "names", ["Angle", "y"])
+    final_show(save_figure, plot, figname, x_label, y_label, "Actor phase portrait", "/plots/")
+
+
+
+
+
+
+def plot_nd_critic(model, env, plot=True, figname="vfunction.pdf", foldername="/plots/", save_figure=True) -> None:
+    if env.observation_space.shape[0] <= 2:
+        raise (ValueError("Observation space dimension {}, should be > 2".format(env.observation_space.shape[0])))
+    definition = 100
+    portrait = np.zeros((definition, definition))
+    state_min = env.observation_space.low
+    state_max = env.observation_space.high
+
+    for index_x, x in enumerate(np.linspace(state_min[0], state_max[0], num=definition)):
+        for index_y, y in enumerate(np.linspace(state_min[1], state_max[1], num=definition)):
+            obs = np.array([[x, y]])
+            for _ in range(2, len(state_min)):
+                z = random.random() - 0.5
+                obs = np.append(obs, z)
+            with th.no_grad():
+                action = model.policy.forward(obs=obs_as_tensor(obs, model.device))
+                value = model.predict_values(obs_as_tensor(obs, model.device), action)
+            portrait[definition - (1 + index_y), index_x] = value.item()
+
+    plt.figure(figsize=(10, 10))
+    plt.imshow(portrait, cmap="inferno", extent=[state_min[0], state_max[0], state_min[1], state_max[1]], aspect="auto")
+    plt.colorbar(label="critic value")
+    plt.scatter([0], [0])
+    x_label, y_label = getattr(env.observation_space, "names", ["x", "y"])
+    final_show(save_figure, plot, figname, x_label, y_label, "V Function", foldername)
+
+
+
+
+
+def state_reward(model, env, deterministic, plot=True, figname="nd_actor.pdf", save_figure=True) -> None:
+
+    if env.observation_space.shape[0] <= 2:
+        raise (ValueError("Observation space dimension {}, should be > 2".format(env.observation_space.shape[0])))
+    definition = 100
+    state_min = env.observation_space.low
+    state_max = env.observation_space.high
+    listeRewards = []
+    
+    for index_x, x in enumerate(np.linspace(state_min[4], state_max[4], num=definition)):
+        obs = env.reset()
+        obs[2] = x
+        print(index_x)
+
+        mean_reward = 0
+        while True:
+            action, _states = model.predict(obs)
+            obs, rewards, dones, info = env.step(action)
+            mean_reward += rewards
+            if dones:
+                env.close()
+                break
+        listeRewards.append(mean_reward)
+
+
+
+    plt.plot([i for i in range(len(listeRewards))], listeRewards, color='b')
+    plt.xlabel("Angle (sur 100 pas : -1 à 1)")
+    plt.ylabel("Total Rewards")
+
+    if save_figure:
+        directory = os.getcwd() + "/dataVizu/"
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        plt.savefig(directory + "plot_state_rewards")
+    plt.show()
+    plt.close()
+    
